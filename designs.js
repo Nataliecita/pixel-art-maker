967eb716e6317298f35f44335e3fb209cb0463db
Natalie Ruiz <natnatruiz@gmail.com>
2018-02-08 21:22:18 +0100

    feat: Change data type

--- designs.js
+++ designs.js
@@ -6,15 +6,15 @@
 
 /**
  * Represents Grid
- *
  * @param {int} height - The height of the grid.
  * @param {int} width - The width of the grid.
  */
 function makeGrid(height, width) {
 
 // Your code goes here!
-console.log(height);
-console.log(width + 'this is the width');
+console.log(height + 'this is the height');
+console.log(typeof width);
+
 
 
 }
@@ -26,8 +26,11 @@ dimensions.addEventListener("submit", function(evt) {
     evt.preventDefault();
     console.log("wooohoo form submitted");
 
-    const height = document.getElementById('inputHeight').value
-    const width = document.getElementById('inputWidth').value
+    let height = document.getElementById('inputHeight').value
+    let width = document.getElementById('inputWidth').value
+
+    height = Number(height);
+    width = Number(width)
 
     makeGrid(height, width);
 })
